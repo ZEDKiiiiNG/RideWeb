af415cb6c8e5b457d582c5ec22a3d261511f0e67
ZEDKIIIING <296213663@qq.com>
2022-01-27 10:21:22 +0800

    register complete

--- ride/views.py
+++ ride/views.py
@@ -17,7 +17,7 @@ from django.http import HttpResponse
 #     return HttpResponse("You're voting on question %s." % question_id)
 
 from django.shortcuts import render,redirect
-from .forms import UserForm
+from .forms import UserForm,RegisterForm
 from . import models
 
 def index(request):
@@ -52,18 +52,50 @@ def login(request):
         return render(request, 'login/login.html', locals())
 
 def register(request):
-    pass
-    return render(request,'login/register.html')
+    if request.session.get('is_login', None):
+        # if already signed in, then cannot register
+        return redirect("/index/")
+    if request.method == "POST":
+        register_form = RegisterForm(request.POST)
+        message = "Please check the content！"
+        if register_form.is_valid():  # get the detaile data
+            username = register_form.cleaned_data['username']
+            password1 = register_form.cleaned_data['password1']
+            password2 = register_form.cleaned_data['password2']
+            email = register_form.cleaned_data['email']
+            if password1 != password2:  # pass word are not the same
+                message = "The passwords are not the same！"
+                return render(request, 'login/register.html', locals())
+            else:
+                same_name_user = models.User.objects.filter(name=username)
+                if same_name_user:  # found the same user name
+                    message = 'user already exists！'
+                    return render(request, 'login/register.html', locals())
+                same_email_user = models.User.objects.filter(email=email)
+                if same_email_user:  #  found the same email
+                    message = 'email already exits！'
+                    return render(request, 'login/register.html', locals())
+
+                # all rules passed
+
+                new_user = models.User.objects.create()
+                new_user.name = username
+                new_user.password = password1
+                new_user.email = email
+                new_user.save()
+                return redirect('/login/')  # return to login page
+    register_form = RegisterForm()
+    return render(request, 'login/register.html', locals())
 
 # log out in login page
 def logout(request):
     if not request.session.get('is_login', None):
         # 如果本来就未登录，也就没有登出一说
-        return redirect('login/login.html')
+        return redirect('/login/')
     request.session.flush()
     # 或者使用下面的方法
     # del request.session['is_login']
     # del request.session['user_id']
     # del request.session['user_name']
     # return redirect("/index/")
-    return redirect('login/login.html')
+    return redirect('/login/')
